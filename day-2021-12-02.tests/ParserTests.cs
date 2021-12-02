400ce4143f330e88a83901d42b744e54df207300
tarpaha <ppopchikovsky@gmail.com>
2021-12-02 12:42:42 +0300

    parser with tests

---
+++ day-2021-12-02.tests/ParserTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace day_2021_12_02.tests
+{
+    public class ParserTests
+    {
+        [TestCase("forward 5", 5, 0)]
+        [TestCase("down 4", 0, 4)] 
+        [TestCase("up 3", 0, -3)]
+        public void Parser_Works_Right(string line, int x, int y)
+        {
+            var commands = Parser.Parse(line).ToList();
+            Assert.That(commands.Count, Is.EqualTo(1));
+            Assert.That(commands.First(), Is.EqualTo((x, y)));
+        }
+    }
+}
---
+++ day-2021-12-02/Parser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace day_2021_12_02
+{
+    public static class Parser
+    {
+        public static IEnumerable<(int x, int y)> Parse(string data)
+        {
+            return data
+                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .Select(line => line.Split(' '))
+                .Select(parts => (
+                    step: parts[0] switch
+                    {
+                        "forward" => (x: 1, y:  0),
+                        "down"    => (x: 0, y: +1),
+                        "up"      => (x: 0, y: -1),
+                        _ => throw new ArgumentOutOfRangeException($"Invalid direction: {parts[0]}")
+                    },
+                    distance: int.Parse(parts[1])))
+                .Select(move => (move.step.x * move.distance, move.step.y * move.distance));
+        }
+    }
+}

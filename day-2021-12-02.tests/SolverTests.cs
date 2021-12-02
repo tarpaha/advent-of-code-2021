7ca04a723c000a406ce6eea4c5c359b28942af0a
tarpaha <ppopchikovsky@gmail.com>
2021-12-02 12:57:39 +0300

    day-2021-12-02 part1

--- day-2021-12-02.tests/SolverTests.cs
+++ day-2021-12-02.tests/SolverTests.cs
@@ -4,10 +4,20 @@ namespace day_2021_12_02.tests
 {
     public class SolverTests
     {
+        private const string Data = @"
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+
+        private const int Part1Result = 150;
+        
         [Test]
         public void Part1()
         {
-            Assert.That(Solver.Part1(), Is.Null);
+            Assert.That(Solver.Part1(Parser.Parse(Data)), Is.EqualTo(Part1Result));
         }
 
         [Test]
--- day-2021-12-02/Solver.cs
+++ day-2021-12-02/Solver.cs
@@ -1,10 +1,14 @@
-﻿namespace day_2021_12_02
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace day_2021_12_02
 {
     public static class Solver
     {
-        public static object Part1()
+        public static int Part1(IEnumerable<(int x, int y)> commands)
         {
-            return null!;
+            var (x, y) = commands.Aggregate((c1, c2) => (c1.x + c2.x, c1.y + c2.y));
+            return x * y;
         }
         
         public static object Part2()

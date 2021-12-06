f021ba5bd9f6c85160ec3ca24c7aecd5172f8312
tarpaha <ppopchikovsky@gmail.com>
2021-12-06 15:14:56 +0300

    day-2021-12-05 part1

--- day-2021-12-05.tests/SolverTests.cs
+++ day-2021-12-05.tests/SolverTests.cs
@@ -4,10 +4,22 @@ namespace day_2021_12_05.tests
 {
     public class SolverTests
     {
+        private const string Data = @"
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2";
+        
         [Test]
         public void Part1()
         {
-            Assert.That(Solver.Part1(), Is.Null);
+            Assert.That(Solver.Part1(Parser.Parse(Data)), Is.EqualTo(5));
         }
         
         [Test]
--- day-2021-12-05/Solver.cs
+++ day-2021-12-05/Solver.cs
@@ -1,10 +1,52 @@
+using System.Linq;
+using System.Collections.Generic;
+
 namespace day_2021_12_05
 {
     public static class Solver
     {
-        public static object Part1()
+        public static int Part1(IEnumerable<Line> lines)
         {
-            return null!;
+            var field = new Dictionary<(int x, int y), int>();
+            foreach (var line in lines)
+            {
+                if (line.X1 == line.X2)
+                {
+                    var x = line.X1;
+                    var (start, finish) = line.Y1 < line.Y2
+                        ? (line.Y1, line.Y2)
+                        : (line.Y2, line.Y1);
+                    for (var y = start; y <= finish; y++)
+                    {
+                        UpdateField(field, x, y);
+                    }
+                }
+                else if (line.Y1 == line.Y2)
+                {
+                    var y = line.Y1;
+                    var (start, finish) = line.X1 < line.X2
+                        ? (line.X1, line.X2)
+                        : (line.X2, line.X1);
+                    for (var x = start; x <= finish; x++)
+                    {
+                        UpdateField(field, x, y);
+                    }
+                }
+            }
+
+            return field.Values.Count(v => v >= 2);
+        }
+
+        private static void UpdateField(IDictionary<(int x, int y), int> field, int x, int y)
+        {
+            if (field.TryGetValue((x, y), out var counter))
+            {
+                field[(x, y)] = counter + 1;
+            }
+            else
+            {
+                field.Add((x, y), 1);
+            }
         }
 
         public static object Part2()

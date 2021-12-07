27110266c6ebcc4d2c8ef697118653952c60bc83
tarpaha <ppopchikovsky@gmail.com>
2021-12-07 10:28:46 +0300

    parser with tests

---
+++ day-2021-12-07.tests/ParserTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace day_2021_12_07.tests
+{
+    public class ParserTests
+    {
+        [Test]
+        public void Parser_Works_Correctly()
+        {
+            Assert.That(
+                Parser.Parse("16,1,2,0,4,2,7,1,2,14"),
+                Is.EquivalentTo(new [] { 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 }));
+        }
+    }
+}
---
+++ day-2021-12-07/Parser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day_2021_12_07
+{
+    public static class Parser
+    {
+        public static IEnumerable<int> Parse(string data)
+        {
+            return data.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
+        }
+    }
+}

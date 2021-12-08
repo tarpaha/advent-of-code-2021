4a9e4d9b395a819269cf6ce08b7ab9398978d03b
tarpaha <ppopchikovsky@gmail.com>
2021-12-08 12:17:42 +0300

    day-2021-12-08 start

---
+++ day-2021-12-08/Solver.cs
@@ -0,0 +1,15 @@
+namespace day_2021_12_08
+{
+    public static class Solver
+    {
+        public static object Part1()
+        {
+            return null!;
+        }
+
+        public static object Part2()
+        {
+            return null!;
+        }
+    }
+}

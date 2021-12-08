cf5495676ecb9941e1d54e078176464df3f4bfd1
tarpaha <ppopchikovsky@gmail.com>
2021-12-08 13:40:07 +0300

    day-2021-12-08 part1

--- day-2021-12-08/Solver.cs
+++ day-2021-12-08/Solver.cs
@@ -7,13 +7,8 @@ namespace day_2021_12_08
     {
         public static int Part1(IEnumerable<Entry> entries)
         {
-            var result = 0;
             var lookingLengths = new HashSet<int> { 2, 4, 3, 7 };
-            foreach (var entry in entries)
-            {
-                result += entry.Output.Count(s => lookingLengths.Contains(s.Length));
-            }
-            return result;
+            return entries.Sum(entry => entry.Output.Count(s => lookingLengths.Contains(s.Length)));
         }
 
         public static object Part2()

08ce7002ebafb8f33fbb183e2b5f85651868cdd8
tarpaha <ppopchikovsky@gmail.com>
2021-12-10 17:50:15 +0300

    day-2021-12-10 part1

--- day-2021-12-10.tests/SolverTests.cs
+++ day-2021-12-10.tests/SolverTests.cs
@@ -4,17 +4,45 @@ namespace day_2021_12_10.tests;
 
 public class SolverTests
 {
-    private const string Data = @"";
+    private const string Data = @"
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]";
 
+    [TestCase("()", null)]
+    [TestCase("[]", null)]
+    [TestCase("([])", null)]
+    [TestCase("{()()()}", null)]
+    [TestCase("<([{}])>", null)]
+    [TestCase("[<>({}){}[([])<>]]", null)]
+    [TestCase("(((((((((())))))))))", null)]    
+    [TestCase("[({(<(())[]>[[{[]{<()<>>", null)]
+    [TestCase("{([(<{}[<>[]}>{[]{[(<()>", '}')]
+    [TestCase("[[<[([]))<([[{}[[()]]]", ')')]
+    [TestCase("[{[{({}]{}}([{[{{{}}([]", ']')]
+    [TestCase("[<(<(<(<{}))><([]([]()", ')')]
+    [TestCase("<{([([[(<>()){}]>(<<{{", '>')]
+    public void GetInvalidChar_Works_Correctly(string line, char? result)
+    {
+        Assert.That(Solver.GetInvalidChar(line), Is.EqualTo(result));
+    }
+    
     [Test]
     public void Part1()
     {
-        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.Null);
+        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.EqualTo(26397));
     }
     
     [Test]
     public void Part2()
     {
-        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.Null);
+        Assert.That(Solver.Part2(Parser.Parse(Data)), Is.Null);
     }
 }
--- day-2021-12-10/Solver.cs
+++ day-2021-12-10/Solver.cs
@@ -4,7 +4,42 @@ public static class Solver
 {
     public static object Part1(Data data)
     {
-        return null!;
+        var scores = new Dictionary<char, int>()
+        {
+            [')'] = 3,
+            [']'] = 57,
+            ['}'] = 1197,
+            ['>'] = 25137
+        };
+        return data.Lines
+            .AsParallel()
+            .Select(GetInvalidChar)
+            .Where(ch => ch.HasValue)
+            .Select(ch => scores[ch!.Value])
+            .Sum();
+    }
+
+    public static char? GetInvalidChar(string line)
+    {
+        var stack = new Stack<char>();
+        foreach (var ch in line)
+        {
+            switch (ch)
+            {
+                case '(' or '[' or '{' or '<':
+                    stack.Push(ch);
+                    break;
+                case ')' when stack.Pop() != '(':
+                    return ')';
+                case ']' when stack.Pop() != '[':
+                    return ']';
+                case '}' when stack.Pop() != '{':
+                    return '}';
+                case '>' when stack.Pop() != '<':
+                    return '>';
+            }
+        }
+        return null;
     }
 
     public static object Part2(Data data)

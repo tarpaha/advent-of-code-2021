da2a1cb124412f742535079eae6ef8b084c26b3e
tarpaha <ppopchikovsky@gmail.com>
2021-12-12 20:56:35 +0300

    day-2021-12-12 part2

--- day-2021-12-12.app/Solution.cs
+++ day-2021-12-12.app/Solution.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using utils;
 
 namespace day_2021_12_12.app;
@@ -9,8 +8,6 @@ public class Solution : ISolution
     {
         var solution = new Solution();
         Console.WriteLine($"Part1: {solution.SolvePart1()}");
-
-        var t = Stopwatch.StartNew();
         Console.WriteLine($"Part2: {solution.SolvePart2()}");
     }
 

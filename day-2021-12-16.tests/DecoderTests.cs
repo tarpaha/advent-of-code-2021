df252f4856aa7bb8e072b0b1529606580cd08bba
tarpaha <ppopchikovsky@gmail.com>
2021-12-16 14:36:58 +0300

    decode hex to binary with tests

---
+++ day-2021-12-16.tests/DecoderTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace day_2021_12_16.tests;
+
+public class DecoderTests
+{
+    [TestCase("4", "0100")]
+    [TestCase("D", "1101")]
+    [TestCase("D2FE28", "110100101111111000101000")]
+    [TestCase("38006F45291200", "00111000000000000110111101000101001010010001001000000000")]
+    public void ConvertHexStringToBinary_Works_Correctly(string str, string resultStr)
+    {
+        var result = resultStr.Select(ch => ch - '0').ToList();
+        Assert.That(Decoder.ConvertHexStringToBinary(str), Is.EquivalentTo(result));
+    }
+}
---
+++ day-2021-12-16/Decoder.cs
@@ -0,0 +1,18 @@
+﻿namespace day_2021_12_16;
+
+public static class Decoder
+{
+    public static int[] ConvertHexStringToBinary(string str)
+    {
+        var bits = new int[str.Length * 4];
+        var p = 0;
+        foreach (var number in str.Select(ch => ch <= '9' ? ch - '0' : ch - 'A' + 10))
+        {
+            bits[p++] = (number & 0b1000) != 0 ? 1 : 0; 
+            bits[p++] = (number & 0b0100) != 0 ? 1 : 0; 
+            bits[p++] = (number & 0b0010) != 0 ? 1 : 0; 
+            bits[p++] = (number & 0b0001) != 0 ? 1 : 0;
+        }
+        return bits;
+    }
+}

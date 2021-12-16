9aaa143a6c6dc4e660e9758d316710031248b3b3
tarpaha <ppopchikovsky@gmail.com>
2021-12-16 16:30:42 +0300

    day-2021-12-16 part1

--- day-2021-12-16.tests/SolverTests.cs
+++ day-2021-12-16.tests/SolverTests.cs
@@ -4,17 +4,18 @@ namespace day_2021_12_16.tests;
 
 public class SolverTests
 {
-    private const string Data = @"";
-
-    [Test]
-    public void Part1()
+    [TestCase("8A004A801A8002F478", 16)]
+    [TestCase("620080001611562C8802118E34", 12)]
+    [TestCase("C0015000016115A2E0802F182340", 23)]
+    [TestCase("A0016C880162017C3686B18A3D4780", 31)]
+    public void Part1(string data, int result)
     {
-        Assert.That(Solver.Part1(Parser.Parse(Data)), Is.Null);
+        Assert.That(Solver.Part1(Parser.Parse(data)), Is.EqualTo(result));
     }
     
     [Test]
     public void Part2()
     {
-        Assert.That(Solver.Part2(Parser.Parse(Data)), Is.Null);
+        Assert.Pass();
     }
 }
--- day-2021-12-16/Solver.cs
+++ day-2021-12-16/Solver.cs
@@ -2,9 +2,20 @@ namespace day_2021_12_16;
 
 public static class Solver
 {
-    public static object Part1(Data data)
+    public static int Part1(Data data)
     {
-        return null!;
+        var packet = Decoder.Decode(data.Message);
+        return SumVersionNumbers(packet);
+    }
+
+    private static int SumVersionNumbers(Packet packet)
+    {
+        return packet switch
+        {
+            LiteralPacket literalPacket => literalPacket.Version,
+            OperatorPacket operatorPacket => operatorPacket.Version + operatorPacket.Packets.Sum(SumVersionNumbers),
+            _ => throw new ArgumentOutOfRangeException(nameof(packet))
+        };
     }
 
     public static object Part2(Data data)

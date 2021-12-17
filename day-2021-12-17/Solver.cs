843910d16e21e24393f5b0d684fcbbe0d1827a51
tarpaha <ppopchikovsky@gmail.com>
2021-12-17 18:54:29 +0300

    day-2021-12-17 part1

--- day-2021-12-17/Solver.cs
+++ day-2021-12-17/Solver.cs
@@ -4,13 +4,32 @@ public static class Solver
 {
     public static object Part1(Data data)
     {
-        var suitableXVelocities = GetSuitableXVelocities(data);
-        return null!;
+        // there is "candles" when x velocity become 0 in target x range
+        // and probe falls vertically
+        
+        // probe always be at y = 0 during fall
+        
+        // and velocity cannot be greater than lowest Y because in this
+        // case probe will overshoot target area with very first step from y = 0
+        
+        // shot vertical speed is fall through zero speed minus 1
+        
+        var vy = -data.YMin - 1;
+
+        // calculate shot height 
+        
+        var y = 0;
+        while(vy != 0)
+        {
+            y += vy;
+            vy -= 1;
+        }
+        return y;
     }
 
-    private static IEnumerable<(int v, IEnumerable<(int t, int x)>)> GetSuitableXVelocities(Data data)
+    private static IEnumerable<(int v, IEnumerable<(int t, int x, int vx)>)> GetSuitableXVelocities(Data data)
     {
-        var suitableX = new List<(int v, IEnumerable<(int t, int x)>)>();
+        var suitableX = new List<(int, IEnumerable<(int, int, int)>)>();
         
         var maxSuitableVelocity = data.XMax;
         for (var initialVelocity = maxSuitableVelocity; initialVelocity > 0; initialVelocity--)
@@ -40,10 +59,10 @@ public static class Solver
 
             if (inTargetArea)
             {
-                var timePos = new List<(int, int)>();
+                var timePos = new List<(int, int, int)>();
                 while (true)
                 {
-                    timePos.Add((t, x));
+                    timePos.Add((t, x, velocity));
                     if (velocity == 0)
                         break;
                     t += 1;
